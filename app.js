2a81ba3184525005daa2f0340b16bd38da37de28
kobyrthr-dev <kobyrthr@gmail.com>
2021-12-18 15:33:21 -0500

    commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const questions = [
         artist:"The Singers Unlimited",
         embed:'<iframe width="560" height="315" src="https://www.youtube.com/embed/vu__E2zpT9Y?start=72" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
         answers: ['Drake. Champagne Poetry','Sampha. Incomplete Kisses','J.Cole.She\'s Mine','Kanye West. Blood on the Leaves'],
-        correct: 0,
+        correct: 0
 
     },
 
@@ -15,30 +15,85 @@ const questions = [
         id:1,
         title:"As Long as I've Got You",
         artist:"The Charmels",
-        embed:'<iframe width="560" height="315" src="https://www.youtube.com/embed/vu__E2zpT9Y?start=72" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        embed:'<iframe width="560" height="315" src="https://www.youtube.com/embed/gp9uZjPaB4w?start=18" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
         answers: ['Sampha. Incomplete Kisses','J.Cole.She\'s Mine','Kanye West. Blood on the Leaves','Wu-Tang Clan. C.R.E.A.M'],
-        correct: 3,
+        correct: 3
 
     },
 
     {
         id:2,
-        title:"Random",
-        artist:"Random",
-        embed:'<iframe width="560" height="315" src="https://www.youtube.com/embed/vu__E2zpT9Y?start=72" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-        answers: ['Random 1','Random 2','Random 3','Random 4'],
-        correct: 2,
+        title:"Pressure",
+        artist:"Quadron",
+        embed:'<iframe width="560" height="315" src="https://www.youtube.com/embed/ILPNhmg3geM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        answers: ['A$AP Rocky. Sundress','Lemon Pepper Freestyle','Kanye West. All Falls Down','Giveon. Like I Want You'],
+        correct: 1
 
     },
     {
         id:3,
-        title:"God bless america",
-        artist:"land of the free supposedly",
-        embed:'<iframe width="560" height="315" src="https://www.youtube.com/embed/vu__E2zpT9Y?start=72" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-        answers: ['asdf','asdf 2','asdf 3','asdf 4'],
-        correct: 2,
+        title:"Ronnie Foster",
+        artist:"Mystic Brew",
+        embed:'<iframe width="560" height="315" src="https://www.youtube.com/embed/VRVanOlsxzQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        answers: ['Common. GO!','J. Cole. Forbidden Fruit','UGK, Outkast. Int\'l Players Anthem','Mural. Lupe Fiasco'],
+        correct: 1
 
-    }
+    },
+    {
+        id:4,
+        title:"Olinga",
+        artist:"Milt Jackson",
+        embed:'<iframe width="560" height="315" src="https://www.youtube.com/embed/i8l_o2R7w84" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        answers: ['Azealia Banks. 212','Erykah Badu. Didn\'t Cha Know','Mos Def. UMI Says','A Tribe Called Quest. Award Tour'],
+        correct:3
+
+    },
+    {
+        id:5,
+        title:"Shape of My Heart",
+        artist:"Sting",
+        embed:'<iframe width="560" height="315" src="https://www.youtube.com/embed/NlwIDxCjL-8?start=10" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        answers: ['Mos Def. Hip Hop','Nas. The Message','Souls of Mischief. 93\' Til Infinity','Craig Mack. Flava in Ya Ear'],
+        correct: 1
+
+    },
+    {
+        id:6,
+        title:"Chanson D\'Un Jour D\'Hiver",
+        artist:"Cortex",
+        embed:'<iframe width="560" height="315" src="https://www.youtube.com/embed/ngcTfQBQjF8?start=43" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        answers: ['Kanye West. Gorgeous','Lupe Fiasco. Mural','UGK, Outkast. Int\'l Players Anthem','Mos Def. Mathematics'],
+        correct: 1
+
+    },
+    {
+        id:7,
+        title:"Mary Jane",
+        artist:"Rick James",
+        embed:'<iframe width="560" height="315" src="https://www.youtube.com/embed/iL4Ig5YdvbI?start=388" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        answers: ['Kanye West. Runaway','Kendrick Lamar. HiiPower','Frank Ocean. In My Room','Giveon. Like I Want You'],
+        correct: 0
+
+    },
+    {
+        id:8,
+        title:"Knee Socks",
+        artist:"Arctic Monkeys",
+        embed:'<iframe width="560" height="315" src="https://www.youtube.com/embed/R7A1mIdiheE?start=159" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        answers: ['A$AP Rocky, Skepta. Praise The Loard (Da Shine)','J.Cole. Middle Child','Pusha T. If You Know You Know','Nipsey Hussle. Victory Lap'],
+        correct: 3
+
+    },
+    {
+        id:9,
+        title:"Don't Let Your Love Fade Away",
+        artist:"Gene Williams",
+        embed:'<iframe width="560" height="315" src="https://www.youtube.com/embed/Zg2K5yWHqF8?start=104" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        answers: ['Jay Electronic. Exhibit C','J. Cole. Forbidden Fruit','Rick Ross, Jay-Z. The Devil Is A Lie','A$AP Rocky, Skepta. Praise The Loard (Da Shine)'],
+        correct:2 
+
+    },
+    
 ]
 let questionPile = questions
 let graveyard = []
