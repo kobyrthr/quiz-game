b9407410e1fa36399def18962e5e7a87e15d7774
kobyrthr-dev <kobyrthr@gmail.com>
2021-12-23 20:38:10 -0500

    restyling

--- app.js
+++ app.js
@@ -6,94 +6,132 @@ const questions = [
     {
         id:0,
         title:"Michelle ",
-        artist:" - The Singers Unlimited",
+        artist:"- The Singers Unlimited",
         embed:`<iframe width=${boxWidth} height="315" src="https://www.youtube.com/embed/vu__E2zpT9Y?start=72" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
         answers: ['Drake. Champagne Poetry','Sampha. Incomplete Kisses','J.Cole.She\'s Mine','Kanye West. Blood on the Leaves'],
-        correct: 0
+        correct: 0,
+        hint:`"And nothing tell the truth like the eyes will
+        Lived so much for others, don't remember how I feel
+        Friends in high places and friends that I hide still
+        Still managed to moonwalk straight through a minefield"`
 
     },
 
     {
         id:1,
         title:"As Long as I've Got You ",
-        artist:" - The Charmels",
+        artist:"- The Charmels",
         embed:`<iframe width=${boxWidth} height="315" src="https://www.youtube.com/embed/gp9uZjPaB4w?start=18" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
         answers: ['Sampha. Incomplete Kisses','J.Cole.She\'s Mine','Kanye West. Blood on the Leaves','Wu-Tang Clan. C.R.E.A.M'],
-        correct: 3
+        correct: 3,
+        hint:`"Though I don't know why I chose to smoke sess
+        I guess that's the time when I'm not depressed
+        But I'm still depressed, and I ask, "What's it worth?"
+        Ready to give up so I seek the old Earth"`
 
     },
 
     {
         id:2,
         title:"Pressure ",
-        artist:" - Quadron",
+        artist:"- Quadron",
         embed:`<iframe width=${boxWidth} height="315" src="https://www.youtube.com/embed/ILPNhmg3geM?start=203" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
         answers: ['A$AP Rocky. Sundress','Drake. Lemon Pepper Freestyle','Kanye West. All Falls Down','Giveon. Like I Want You'],
-        correct: 1
+        correct: 1,
+        hint: `"If this is your hobby, then come and meet your maker
+        Champagne, ring bells in the streets of Jamaica
+        Started at the crib, look how far this shit'll take ya
+        Ross sittin' on two hunnid thirty-five acres"`
 
     },
     {
         id:3,
         title:"Ronnie Foster",
-        artist:" - Mystic Brew",
+        artist:"- Mystic Brew",
         embed:`<iframe width=${boxWidth} height="315" src="https://www.youtube.com/embed/VRVanOlsxzQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
         answers: ['Common. GO!','J. Cole. Forbidden Fruit','UGK, Outkast. Int\'l Players Anthem','Mural. Lupe Fiasco'],
-        correct: 1
+        correct: 1,
+        hint:`"Take a seat baby girl you've been all in my mind
+        I know I ain't called gotta pardon my grind
+        Just copped a maroon 5, no Adam Levine
+        Became a man by myself, only father was time"`
 
     },
     {
         id:4,
         title:"Olinga",
-        artist:" - Milt Jackson",
+        artist:"- Milt Jackson",
         embed:`<iframe width=${boxWidth} height="315" src="https://www.youtube.com/embed/i8l_o2R7w84?start=43" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
         answers: ['Azealia Banks. 212','Erykah Badu. Didn\'t Cha Know','Mos Def. UMI Says','A Tribe Called Quest. Award Tour'],
-        correct:3
+        correct:3,
+        hint:`"Coming with more hits than the Braves and the Yankees
+        Living mad phat like an oversized mampi
+        The wackest crews try to diss, it makes me laugh
+        When my track record's longer than a DC-20 aircraft"`
 
     },
     {
         id:5,
         title:"Shape of My Heart",
-        artist:" - Sting",
+        artist:"- Sting",
         embed:`<iframe width=${boxWidth} height="315" src="https://www.youtube.com/embed/NlwIDxCjL-8?start=10" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
         answers: ['Mos Def. Hip Hop','Nas. The Message','Souls of Mischief. 93\' Til Infinity','Craig Mack. Flava in Ya Ear'],
-        correct: 1
+        correct: 1,
+        hint:`"The highlights of living, Vegas style roll dice in linen
+        Antera spinning on milleniums, twenty G bets I'm winning them"`
 
     },
     {
         id:6,
         title:"Chanson D\'Un Jour D\'Hiver",
-        artist:" - Cortex",
+        artist:"- Cortex",
         embed:`<iframe width=${boxWidth} height="315" src="https://www.youtube.com/embed/ngcTfQBQjF8?start=43" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
         answers: ['Kanye West. Gorgeous','Lupe Fiasco. Mural','UGK, Outkast. Int\'l Players Anthem','Mos Def. Mathematics'],
-        correct: 1
+        correct: 1,
+        hint:`"I like my pancakes cut in swirls
+        Morrocan moles and undercover squirrels
+        I like cartoons, southern cities with large moons
+        Faith healers, ex-female drug dealers and art booms"`
 
     },
     {
         id:7,
         title:"Mary Jane",
-        artist:" - Rick James",
+        artist:"- Rick James",
         embed:`<iframe width=${boxWidth} height="315" src="https://www.youtube.com/embed/iL4Ig5YdvbI?start=388" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
         answers: ['Kanye West. Runaway','Kendrick Lamar. HiiPower','Frank Ocean. In My Room','Giveon. Like I Want You'],
-        correct: 0
+        correct: 0,
+        hint:`"Never was much of a romantic
+        I could never take the intimacy
+        And I know it did damage
+        'Cause the look in your eyes is killin' me"`
 
     },
     {
         id:8,
         title:"Knee Socks",
-        artist:" - Arctic Monkeys",
+        artist:"- Arctic Monkeys",
         embed:`<iframe width=${boxWidth} height="315" src="https://www.youtube.com/embed/R7A1mIdiheE?start=159" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
         answers: ['A$AP Rocky, Skepta. Praise The Loard (Da Shine)','J.Cole. Middle Child','Pusha T. If You Know You Know','Nipsey Hussle. Victory Lap'],
-        correct: 3
+        correct: 3,
+        hint:`"I'm finna take it there
+        This time around I'ma make it clear
+        Spoke some things into the universe and they appeared
+        I say it's worth it, I won't say it's fair"`
 
     },
     {
         id:9,
         title:"Don't Let Your Love Fade Away",
         
-        artist:" - Gene Williams",
+        artist:"- Gene Williams",
         embed:`<iframe width=${boxWidth} height="315" src="https://www.youtube.com/embed/Zg2K5yWHqF8?start=104" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
         answers: ['Jay Electronic. Exhibit C','J. Cole. Forbidden Fruit','Rick Ross, Jay-Z. The Devil Is A Lie','A$AP Rocky, Skepta. Praise The Loard (Da Shine)'],
-        correct:2 
+        correct:2,
+        hint:`"You seen what I did to the stop and frisk
+        Brooklyn on the Barney's like we own the bitch
+        Give the money to the hood, now we all win
+        Got that Barney's floor lookin' like a VIM"` 
 
     },
     
@@ -115,16 +153,15 @@ const answers = questions[1].answers
 const showScore = document.querySelector("#score")
 const modalBg = document.querySelector('.modalBg')
 const modalMain = document.querySelector('.modalMain')
-
+const hintBtn = document.querySelector('#hintBtn')
+const hintArea = document.querySelector('#hint')
+const hintBox = document.querySelector('#hintBox')
 
 
 //DOM MANIPULATION
 hintTitle.innerHTML = questions[1].title
 hintArtist.innerHTML = questions[1].artist
 songBox.innerHTML = questions[1].embed
-
-
-
 
 
 //FUNCTIONS
@@ -148,10 +185,18 @@ for (x=0;x<currentQuestion.answers.length;x++){
     options[x].innerHTML = currentQuestion.answers[x]
 }
 
-console.log('Question pile',questionPile)
-console.log('Graveyard',graveyard)
-console.log(currentQuestion)
-console.log(currentQuestion.title)
+hintBox.style.display='none'
+hintBtn.style.display='flex'
+
+
+
+
+
+
+// console.log('Question pile',questionPile)
+// console.log('Graveyard',graveyard)
+// console.log(currentQuestion)
+// console.log(currentQuestion.title)
 
 }
 const showAnswer = (e)=>{
@@ -263,21 +308,9 @@ document.querySelector('#answer').addEventListener('click',showAnswer)
 endGame()
 
 
-
-
-
-
-
-
-
-/**
- * I need a function that takes a question set and renders it on the page
- * Need a next button
- * On click of next, render a new question
- * Need the foolowing variables in my build quiz function
-    * quesiton title = questoin[x]
-    * question artist =
-    * question video =
-    * question asnwers =
-    *  
- */
+hintBtn.addEventListener('click',(e)=>{
+    hintBtn.style.display='none'
+    hintBox.innerText = currentQuestion.hint
+    hintBox.style.display="flex"
+    hintBox.style.flexDirection='column'
+})
