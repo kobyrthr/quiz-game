4af63a593616102400b1f7a99d07c1c9ca1d934f
kobyrthr-dev <kobyrthr@gmail.com>
2023-05-16 08:49:24 -0400

    added the Spotify embed

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const questions = [
         id:0,
         title:"Michelle ",
         artist:"- The Singers Unlimited",
-        embed:`<iframe width=${boxWidth} height="315" src="https://www.youtube.com/embed/vu__E2zpT9Y?start=72" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+        embed:`<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/4KsRhsifjqKgnfQHVIgJke?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
         answers: ['Drake. Champagne Poetry','Sampha. Incomplete Kisses','J.Cole.She\'s Mine','Kanye West. Blood on the Leaves'],
         correct: 0,
         hint:`"And nothing tell the truth like the eyes will
@@ -21,7 +21,7 @@ const questions = [
         id:1,
         title:"As Long as I've Got You ",
         artist:"- The Charmels",
-        embed:`<iframe width=${boxWidth} height="315" src="https://www.youtube.com/embed/gp9uZjPaB4w?start=18" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+        embed:`<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/4KsRhsifjqKgnfQHVIgJke?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
         answers: ['Sampha. Incomplete Kisses','J.Cole.She\'s Mine','Kanye West. Blood on the Leaves','Wu-Tang Clan. C.R.E.A.M'],
         correct: 3,
         hint:`"Though I don't know why I chose to smoke sess
@@ -35,7 +35,7 @@ const questions = [
         id:2,
         title:"Pressure ",
         artist:"- Quadron",
-        embed:`<iframe width=${boxWidth} height="315" src="https://www.youtube.com/embed/ILPNhmg3geM?start=203" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+        embed:`<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/4KsRhsifjqKgnfQHVIgJke?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
         answers: ['A$AP Rocky. Sundress','Drake. Lemon Pepper Freestyle','Kanye West. All Falls Down','Giveon. Like I Want You'],
         correct: 1,
         hint: `"If this is your hobby, then come and meet your maker
@@ -48,7 +48,7 @@ const questions = [
         id:3,
         title:"Ronnie Foster",
         artist:"- Mystic Brew",
-        embed:`<iframe width=${boxWidth} height="315" src="https://www.youtube.com/embed/VRVanOlsxzQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+        embed:`<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/4KsRhsifjqKgnfQHVIgJke?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
         answers: ['Common. GO!','J. Cole. Forbidden Fruit','UGK, Outkast. Int\'l Players Anthem','Mural. Lupe Fiasco'],
         correct: 1,
         hint:`"Take a seat baby girl you've been all in my mind
@@ -61,7 +61,7 @@ const questions = [
         id:4,
         title:"Olinga",
         artist:"- Milt Jackson",
-        embed:`<iframe width=${boxWidth} height="315" src="https://www.youtube.com/embed/i8l_o2R7w84?start=43" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+        embed:`<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/4KsRhsifjqKgnfQHVIgJke?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
         answers: ['Azealia Banks. 212','Erykah Badu. Didn\'t Cha Know','Mos Def. UMI Says','A Tribe Called Quest. Award Tour'],
         correct:3,
         hint:`"Coming with more hits than the Braves and the Yankees
@@ -74,7 +74,7 @@ const questions = [
         id:5,
         title:"Shape of My Heart",
         artist:"- Sting",
-        embed:`<iframe width=${boxWidth} height="315" src="https://www.youtube.com/embed/NlwIDxCjL-8?start=10" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+        embed:`<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/4KsRhsifjqKgnfQHVIgJke?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
         answers: ['Mos Def. Hip Hop','Nas. The Message','Souls of Mischief. 93\' Til Infinity','Craig Mack. Flava in Ya Ear'],
         correct: 1,
         hint:`"The highlights of living, Vegas style roll dice in linen
@@ -85,7 +85,7 @@ const questions = [
         id:6,
         title:"Chanson D\'Un Jour D\'Hiver",
         artist:"- Cortex",
-        embed:`<iframe width=${boxWidth} height="315" src="https://www.youtube.com/embed/ngcTfQBQjF8?start=43" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+        embed:`<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/4KsRhsifjqKgnfQHVIgJke?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
         answers: ['Kanye West. Gorgeous','Lupe Fiasco. Mural','UGK, Outkast. Int\'l Players Anthem','Mos Def. Mathematics'],
         correct: 1,
         hint:`"I like my pancakes cut in swirls
@@ -98,7 +98,7 @@ const questions = [
         id:7,
         title:"Mary Jane",
         artist:"- Rick James",
-        embed:`<iframe width=${boxWidth} height="315" src="https://www.youtube.com/embed/iL4Ig5YdvbI?start=388" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+        embed:`<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/4KsRhsifjqKgnfQHVIgJke?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
         answers: ['Kanye West. Runaway','Kendrick Lamar. HiiPower','Frank Ocean. In My Room','Giveon. Like I Want You'],
         correct: 0,
         hint:`"Never was much of a romantic
@@ -111,7 +111,7 @@ const questions = [
         id:8,
         title:"Knee Socks",
         artist:"- Arctic Monkeys",
-        embed:`<iframe width=${boxWidth} height="315" src="https://www.youtube.com/embed/R7A1mIdiheE?start=159" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+        embed:`<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/4KsRhsifjqKgnfQHVIgJke?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
         answers: ['A$AP Rocky, Skepta. Praise The Loard (Da Shine)','J.Cole. Middle Child','Pusha T. If You Know You Know','Nipsey Hussle. Victory Lap'],
         correct: 3,
         hint:`"I'm finna take it there
@@ -125,7 +125,7 @@ const questions = [
         title:"Don't Let Your Love Fade Away",
         
         artist:"- Gene Williams",
-        embed:`<iframe width=${boxWidth} height="315" src="https://www.youtube.com/embed/Zg2K5yWHqF8?start=104" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+        embed:`<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/4KsRhsifjqKgnfQHVIgJke?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
         answers: ['Jay Electronic. Exhibit C','J. Cole. Forbidden Fruit','Rick Ross, Jay-Z. The Devil Is A Lie','A$AP Rocky, Skepta. Praise The Loard (Da Shine)'],
         correct:2,
         hint:`"You seen what I did to the stop and frisk
